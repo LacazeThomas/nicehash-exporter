86d4884549af32c351ee60d8d62d7cd02dc6b6f7
Thomas LACAZE <contact@thomaslacaze.fr>
2021-04-03 15:26:52 +0200

    trigger ci

--- logs.go
+++ logs.go
@@ -5,7 +5,7 @@ import "go.uber.org/zap"
 //Check error with details informations
 func Check(details string, err error) {
 	if err != nil {
-		zap.S().Errorf("Error %s %s\n", details, err.Error())
+		zap.S().Errorf("Error %s %s", details, err.Error())
 	} else {
 		zap.S().Debugf("Success %s", details)
 	}
