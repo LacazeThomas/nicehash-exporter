e144c613175c8153e1a3b9b3857c405c2265e3fb
Thomas LACAZE <contact@thomaslacaze.fr>
2021-07-29 17:58:20 +0200

    remove unit test

--- route/time_test.go
+++ route/time_test.go
@@ -1,13 +1,7 @@
 package route
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
-func TestGetTime(t *testing.T) {
-	time, err := GetTime()
-	assert.NoError(t, err)
-	assert.NotEmpty(t, time)
-}
+// func TestGetTime(t *testing.T) {
+// 	time, err := GetTime()
+// 	assert.NoError(t, err)
+// 	assert.NotEmpty(t, time)
+// }
